748d9d4511f7248e99ddea14bd40ba6630b742d9
Das, P.K. (Pijush Kanti) <pijushdas@gmail.com>
2020-06-02 23:53:17 +0200

    The complete back end.

--- api/src/main/java/com/cts/airticket/controller/AirportController.java
+++ api/src/main/java/com/cts/airticket/controller/AirportController.java
@@ -1,8 +1,55 @@
 package com.cts.airticket.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cts.airticket.dao.AirportRepository;
+import com.cts.airticket.entity.AirportEntity;
+import com.cts.airticket.util.ApiUtil;
+import com.cts.airticket.vo.AirportVo;
 
 @RestController
 public class AirportController {
 
+	@Autowired
+	AirportRepository service;
+
+	@RequestMapping("/findByCode/{airportCode}")
+	List<AirportVo> findByAirportCodeContaining(@PathVariable String airportCode) {
+		List<AirportEntity> airportEntities = service.findByAirportCodeContaining(airportCode);
+
+		if (!CollectionUtils.isEmpty(airportEntities)) {
+			List<AirportVo> vos =  airportEntities
+					.stream()
+					.map(ApiUtil::converFromEntityToVo)
+					.collect(Collectors.toList());
+			return vos;
+					
+		}
+
+		return null;
+	}
+
+	@RequestMapping("/findByDescription/{airportDescription}")
+	List<AirportVo> findByAirportDescriptionContaining(@PathVariable String airportDescription) {
+		List<AirportEntity> airportEntities = service.findByAirportDescriptionContaining(airportDescription);
+
+		if (!CollectionUtils.isEmpty(airportEntities)) {
+
+			List<AirportVo> vos = airportEntities
+					.stream()
+					.map(ApiUtil::converFromEntityToVo)
+					.collect(Collectors.toList());
+					
+			return vos;
+		}
+
+		return null;
+	}
 }
---
+++ api/src/main/java/com/cts/airticket/vo/AirportVo.java
@@ -0,0 +1,50 @@
+package com.cts.airticket.vo;
+
+public class AirportVo {
+
+	private String airportCode;
+	
+
+	private String airportDescription;
+	
+
+	private int airportXCordinate;
+	
+
+	private int airportYCordinate;
+
+	public String getAirportCode() {
+		return airportCode;
+	}
+
+	public void setAirportCode(String airportCode) {
+		this.airportCode = airportCode;
+	}
+
+	public String getAirportDescription() {
+		return airportDescription;
+	}
+
+	public void setAirportDescription(String airportDescription) {
+		this.airportDescription = airportDescription;
+	}
+
+	public int getAirportXCordinate() {
+		return airportXCordinate;
+	}
+
+	public void setAirportXCordinate(int airportXCordinate) {
+		this.airportXCordinate = airportXCordinate;
+	}
+
+	public int getAirportYCordinate() {
+		return airportYCordinate;
+	}
+
+	public void setAirportYCordinate(int airportYCordinate) {
+		this.airportYCordinate = airportYCordinate;
+	}
+
+	
+	
+}
